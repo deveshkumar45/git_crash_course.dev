deb810c733e43b529ab1368cb0dc99cde205c878
devesh kumar <deveshjangde2006@gmail.com>
2025-01-09 16:13:34 +0530

    totally commited

--- man/roll.js
+++ man/roll.js
@@ -1,3 +1,5 @@
 const nameDev = "Devesh kumar"
 const age = 19
 console.log( `my name is: ${nameDev} and my age is: ${age}`);
+
+console.log("Hello World!");
--- stil.js
+++ stil.js
@@ -10,3 +10,10 @@ const newName = {
 
 // Call the method
 newName.printDetails();
+
+let myArry = [1, 2, 3, 4, 5, 6]
+for (let index = 0; index <myArry.length; index++) {
+  const element = myArry[index];
+  console.log(element);
+  
+}
