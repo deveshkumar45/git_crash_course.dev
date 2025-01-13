6bed810e3615ecca9cbf4d4f419d19efbbec8252
devesh kumar <deveshjangde2006@gmail.com>
2025-01-13 23:16:41 +0530

    new commmit is soon

--- stil.js
+++ stil.js
@@ -23,3 +23,9 @@ for (let index = 0; index <meArry.length; index++) {
   console.log(element);
   
 }
+
+let devArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (let index = 0; index < devArray.length; index++) {
+  const element = devArray[index];
+  console.log(element);
+}
